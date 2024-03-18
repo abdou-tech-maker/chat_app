1f8d8a322030ea401149cab3327ad0de704aefcc
Abderrahmane <abderrahmanekihal5@gmail.com>
2024-03-18 23:53:08 +0100

    add needed packages

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,17 @@
+// ignore_for_file: depend_on_referenced_packages
+
+import 'dart:io';
+
+import 'package:chat_task/models/user_model.dart';
 import 'package:chat_task/screens/main_screen.dart';
 import 'package:chat_task/service/local_service.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hive_flutter/adapters.dart';
+import 'package:path_provider/path_provider.dart' as pathProvider;
 
+import 'constantes/bloc_providers.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -12,6 +20,10 @@ void main() async {
     options: DefaultFirebaseOptions.currentPlatform,
   );
   await Hive.initFlutter();
+  Directory directory = await pathProvider.getApplicationDocumentsDirectory();
+  Hive.init(directory.path);
+
+  Hive.registerAdapter(UserAdapter());
   await Hive.openBox('userBox');
 
   initializeUsers();
@@ -23,13 +35,16 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        useMaterial3: true,
+    return MultiBlocProvider(
+      providers: getProvidersArray(context),
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          useMaterial3: true,
+        ),
+        home: const MainScreen(),
       ),
-      home: const MainScreen(),
     );
   }
 }
