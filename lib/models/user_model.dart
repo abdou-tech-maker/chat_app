fc95e6faa7f3af1d40c8b2d48af0e5b539f6c309
Abderrahmane <abderrahmanekihal5@gmail.com>
2024-03-18 23:53:43 +0100

    user model with the generated file

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,14 +1,34 @@
+import 'package:equatable/equatable.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:json_annotation/json_annotation.dart';
 
 part 'user_model.g.dart';
 
+@JsonSerializable()
 @HiveType(typeId: 0)
-class User {
+class User extends Equatable {
   @HiveField(0)
-  final String id;
+  final String? uid;
 
   @HiveField(1)
-  final String name;
+  final String? name;
+  @HiveField(2)
+  final String? email;
+  @HiveField(3)
+  final String? imageUrl;
+  @HiveField(4)
+  final DateTime? lastSeen;
+  @HiveField(5)
+  final bool? isOnline;
+  const User({
+    required this.uid,
+    required this.name,
+    required this.email,
+    required this.imageUrl,
+    required this.lastSeen,
+    required this.isOnline,
+  });
 
-  User({required this.id, required this.name});
+  @override
+  List<Object?> get props => [uid, name, email, imageUrl, isOnline, lastSeen];
 }
