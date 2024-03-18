4e927ca0a27c3612f7c793e14820412169d3a4d0
Abderrahmane <abderrahmanekihal5@gmail.com>
2024-03-18 23:54:14 +0100

    first user list ui

--- lib/screens/first_user.dart
+++ lib/screens/first_user.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/events/user_event.dart';
+import '../bloc/states/user_state.dart';
+import '../bloc/user_bloc.dart';
 
 class FirstUser extends StatefulWidget {
   const FirstUser({super.key});
@@ -9,12 +14,45 @@ class FirstUser extends StatefulWidget {
 
 class _FirstUserState extends State<FirstUser> {
   @override
+  void initState() {
+    super.initState();
+    // context.read<UsersBloc>().add(const UserEvent.loadUsers());
+    BlocProvider.of<UsersBloc>(context).add(const LoadUsers());
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
-    );
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: const Icon(Icons.add),
+        ),
+        body: BlocBuilder<UsersBloc, UserState>(
+          builder: (context, state) {
+            if (state is UsersLoading) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (state is UsersLoaded) {
+              final users = state.users;
+              return ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  final user = users[index];
+                  return ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(user.imageUrl ?? ""),
+                    ),
+                    title: Text(user.name ?? ""),
+                    subtitle: Text(user.email ?? ""),
+                  );
+                },
+              );
+            } else if (state is UsersLoadFailure) {
+              // Display an error message
+              return const Center(child: Text('Failed to load users.'));
+            } else {
+              return const Center(child: Text('Unknown state.'));
+            }
+          },
+        ));
   }
 }
